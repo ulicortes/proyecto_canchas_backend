146e79f976b4c14be32a63a2e3e1f0e05931ac4c
ulicortes <ulisestcortes@gmail.com>
2025-10-02 17:27:47 -0300

    Entorno editado.

--- config/db.js
+++ config/db.js
@@ -1,8 +1,8 @@
 import pg from 'pg'
 const { Pool } = pg
-
+const conn = `${process.env.DATABASE_URL}`;
 const pool = new Pool({
-  connectionString: 'postgresql://postgres.fmgzjzabrjtlmftrciqh:to0bWWDHWvuTLWlv@aws-1-sa-east-1.pooler.supabase.com:6543/postgres',
+  connectionString: conn,
   ssl: {
     rejectUnauthorized: false,
   },
